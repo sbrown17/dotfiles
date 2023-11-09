2b2e5b8a30cf3233c6c5898fd31b7ebdfc80988d
Stuart Brown <stubrown357@gmail.com>
2023-11-08 22:30:56 -0600

    init init.el

---
+++ .emacs.d/init.el
@@ -0,0 +1,45 @@
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(load-theme 'catppuccin :no-confirm)
+(package-refresh-contents)
+
+;; This is to make all those autosave files go to one folder
+;;(setq backup-directory-alist
+;;      `((".*" . ,(expand-file-name
+;;             (concat user-emacs-directory "backups"))))
+;;(setq auto-save-file-name-transforms
+;;      `((".*" ,(expand-file-name
+;;             (concat user-emacs-directory "backups") t)))
+
+;; GET EVIL
+;; Set up package.el to work with MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(package-refresh-contents)
+
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(catppuccin-theme rust-mode darktooth-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
