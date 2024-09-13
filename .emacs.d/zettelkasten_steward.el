9b4373dc17a6a2c763d2f092e254cb0b8fd14b5f
Stuart Brown <stubrown357@gmail.com>
2024-09-13 14:07:08 -0500

    Create zettelkasten_steward.el

---
+++ .emacs.d/zettelkasten_steward.el
@@ -0,0 +1,27 @@
+(setq org-capture-templates
+  '(("z" "Zettelkasten Entry" entry (file create-zettelkasten-entry)
+    "* %?\nEntered on %U\n  %i\n  %a")))
+
+(defun create-zettelkasten-entry ()
+  "Create a dated filename for zettelkasten identification purposes."
+  (let ((name (read-string "Enter the file name: ")))
+    (expand-file-name (format "%s_%s.org"
+      (format-time-string "%Y%m%d%H%M%S")
+      name)
+    "~/Development/notes/")))
+
+(defun get-directory-filenames (directory)
+  "Return a list of filenames in the given Directory."
+    (directory-files directory nil "^[^0-9].*\.org$" t))
+
+;; create-new-name -> get current datetime if current_datetime exists, decrement and check again else append datetime to the old name with an underscore
+
+(defun rename-file-safe (old-name new-name)
+  "Rename file old-name to new-name safely."
+  (condition-case err
+      (progn
+	(when (file-exists-p new-name)
+	  (error "File '%s' already exists" new-name))
+      (rename-file old-name new-name)
+      (message "File renamed from '%s' to '%s'" old-name new-name))
+    (error (message "Error renaming file: %s" (error-message-string err)))))
