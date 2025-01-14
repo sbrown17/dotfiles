bfb9404e5d98ff22bc5c8bc5a130129517476fda
Stuart Brown <stubrown357@gmail.com>
2025-01-13 18:11:57 -0600

    line numbers and font sizing

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,7 @@
+;; general setup
+(global-display-line-numbers-mode)
+(set-face-attribute 'default nil :height 140)
+
 ;; Set up package.el to work with MELPA
 (require 'package)
 (add-to-list 'package-archives
